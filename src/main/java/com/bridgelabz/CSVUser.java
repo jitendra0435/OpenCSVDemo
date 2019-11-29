3203450ff04a5c58370c19977d97d1959cc0f5d5
jitendraB <jitendrabachhav0435@gmail.com>
2019-11-29 17:57:51 +0530

    Write code for reading record from file

---
+++ src/main/java/com/bridgelabz/CSVUser.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVUser {
+    @CsvBindByName
+    private String name;
+
+    @CsvBindByName(column = "email", required = true)
+    private String email;
+
+    @CsvBindByName(column = "phone")
+    private String phoneNo;
+
+    @CsvBindByName
+    private String country;
+}
